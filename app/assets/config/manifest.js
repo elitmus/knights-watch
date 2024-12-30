9373028f21e5f4a80fddd6411c2f339730651b8a
maniSHarma7575 <hacker.sharma7575@gmail.com>
2024-12-30 14:24:42 +0530

    [UPGRADE] Make comptaible with sprockets 4.2.1

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link proctoring_manifest.js
+//= link 100ms_manifest.js
+//= link kurento_manifest.js
+//= link videojs_manifest.js
---
+++ app/assets/config/videojs_manifest.js
@@ -0,0 +1,2 @@
+//= link_directory ../stylesheets/videojs .css
+//= link_directory ../javascripts/videojs .js
